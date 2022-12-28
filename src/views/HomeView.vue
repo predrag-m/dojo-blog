2f8d495eb7f7245a4d851e0dd021f8dbd7cc35c3
Predrag Markovljev <predrag.markovljev@gmail.com>
2022-12-28 22:12:21 +0100

    config: scaffold new vue 3 typescript project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
